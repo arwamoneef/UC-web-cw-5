3f46f365e252ec895f0cc38844f1e8e490a9c868
arwamoneef <arwa.moneef2213@gmail.com>
2023-09-04 19:42:30 +0300

    done

---
+++ script.js
@@ -0,0 +1,21 @@
+alert("مرحباً بك في موقعي انا اروى و سوف احسب درجتك ");
+ let grade =prompt("أدخل درجتك");
+ console.log(grade); 
+ if(grade>= 90 &&grade<=100){
+    console.log("لقد حصلت على امتياز ")
+ }
+ else if(grade>=80 && grade <=90){
+    console.log("لقد حصلت على جيد جداً")
+ }
+ else if(grade>=70 && grade <=79){
+     console.log( "لقد حصلت على جيد ")    
+ }
+ else if(grade>=60 && grade <=79){
+    console.log("لقد حصلت على مقبول ")
+ }
+ else if ( grade>=50 && grade <= 59){
+    console.log("لقد حصلت على ضعيف")
+ }
+ else if(grade>= 50 && grade <= أقل ){
+    console.log(" لقد حصلت على راسب")
+ }
